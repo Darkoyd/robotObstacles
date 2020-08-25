18e56a2bd18a11f3e8edb43feb230b9467795ae6
Nicolás Londoño <nilocuru@hotmail.com>
2020-08-24 22:39:19 -0500

    Finished

--- source/uniandes/lym/robot/control/Interpreter.java
+++ source/uniandes/lym/robot/control/Interpreter.java
@@ -58,11 +58,185 @@ public class Interpreter {
 		}
 	}
 
-	private void move(int n) {
-		world.moveForward(n);
+	private void move(String n) throws Error {
+		world.moveForward(getValueFromString(n));
 	}
 
-	private boolean facing(String o) {
+	private int getValueFromString(String val) {
+		try {
+			return Integer.parseInt(val);
+		} catch (Exception e) {
+			for (int i = 0; i < varNames.size(); i++) {
+			if (val.equals(varNames.get(i))) {
+				return varValues.get(i);
+			}
+		}
+		throw new Error("Unexpected parameter " + val);
+		}
+	}
+
+	private void moveDir(String n, String D) throws Error {
+		int times = getValueFromString(n);
+		int initialO = world.getFacing();
+		switch (initialO) {
+			case 0:
+				if (D.equals("front")) {
+					world.moveForward(times);
+				} else if (D.equals("back")) {
+					world.moveForward(-times);
+				} else if (D.equals("left")) {
+					world.moveHorizontally(-times);
+				} else if (D.equals("right")) {
+					world.moveHorizontally(times);
+				} else {
+					throw new Error("Unexpected parameter: " + D);
+				}
+					break;
+			case 1:
+				if (D.equals("front")) {
+					world.moveForward(-times);
+				} else if (D.equals("back")) {
+					world.moveForward(times);
+				} else if (D.equals("left")) {
+					world.moveHorizontally(times);
+				} else if (D.equals("right")) {
+					world.moveHorizontally(-times);
+				} else {
+					throw new Error("Unexpected parameter: " + D);
+				}
+					break;
+			case 2:
+				if (D.equals("front")) {
+					world.moveHorizontally(times);
+				} else if (D.equals("back")) {
+					world.moveHorizontally(-times);
+				} else if (D.equals("left")) {
+					world.moveVertically(times);
+				} else if (D.equals("right")) {
+					world.moveVertically(-times);
+				} else {
+					throw new Error("Unexpected parameter: " + D);
+				}
+					break;
+			case 3:
+				if (D.equals("front")) {
+					world.moveHorizontally(-times);
+				} else if (D.equals("back")) {
+					world.moveHorizontally(times);
+				} else if (D.equals("left")) {
+					world.moveVertically(-times);
+				} else if (D.equals("right")) {
+					world.moveVertically(times);
+				} else {
+					throw new Error("Unexpected parameter: " + D);
+				}
+					break;
+			default:
+					break;
+		}
+	}
+
+	private void face(String O) throws Error {
+		int initialO = world.getFacing();
+		switch (initialO) {
+			case 0:
+				if (O.equals("north")) {
+					break;
+				} else if (O.equals("south")) {
+					world.turnRight();
+					world.turnRight();
+				} else if (O.equals("east")) {
+					world.turnRight();
+				} else if (O.equals("west")) {
+					world.turnRight();
+					world.turnRight();
+					world.turnRight();
+				} else {
+					throw new Error("Unexpected parameter: " + O);
+				}
+					break;
+			case 1:
+				if (O.equals("north")) {
+					world.turnRight();
+					world.turnRight();
+				} else if (O.equals("south")) {
+					break;
+				} else if (O.equals("east")) {
+					world.turnRight();
+					world.turnRight();
+					world.turnRight();
+				} else if (O.equals("west")) {
+					world.turnRight();
+				} else {
+					throw new Error("Unexpected parameter: " + O);
+				}
+					break;
+			case 2:
+				if (O.equals("north")) {
+					world.turnRight();
+					world.turnRight();
+					world.turnRight();
+				} else if (O.equals("south")) {
+					world.turnRight();
+				} else if (O.equals("east")) {
+					break;
+				} else if (O.equals("west")) {
+					world.turnRight();
+					world.turnRight();
+				} else {
+					throw new Error("Unexpected parameter: " + O);
+				}
+					break;
+			case 3:
+				if (O.equals("north")) {
+					world.turnRight();
+				} else if (O.equals("south")) {
+					world.turnRight();
+					world.turnRight();
+					world.turnRight();
+				} else if (O.equals("east")) {
+					world.turnRight();
+					world.turnRight();
+				} else if (O.equals("west")) {
+					break;
+				} else {
+					throw new Error("Unexpected parameter: " + O);
+				}
+					break;
+			default:
+					break;
+	}
+}
+
+	private void moveInDir(String n, String O) {
+		int steps = getValueFromString(n);
+		face(O);
+		world.moveForward(steps);
+	}
+
+	private boolean not(String method) {
+		if (method.startsWith("facing")) {
+			return !facing(method.replace("facing(", "").replace(")", ""));
+		}
+		else return false;
+	}
+
+	private void turn(String D) throws Error {
+		if (D.equals("left")) {
+			world.turnRight();
+			world.turnRight();
+			world.turnRight();
+		} else if (D.equals("right")) {
+			world.turnRight();
+		} else if (D.equals("around")) {
+			world.turnRight();
+			world.turnRight();
+		} else {
+			throw new Error("Unexpected parameter " + D);
+		}
+	}
+
+	private boolean facing(String o) throws Error {
 		boolean res = false;
 		switch (o) {
 			case "north":
@@ -76,9 +250,74 @@ public class Interpreter {
 			case "west":
 				res = world.facingWest();
 			default:
-				break;
+				throw new Error("Unexpected parameter " + o);
 		}
 		return res;
+	}
+
+	private void pick(String n, String X) throws Error {
+		int num = getValueFromString(n);
+		if (X.equals("Balloons")) {
+			for (int i = 0; i < num; i++) {
+				world.pickupBalloon();
+			}
+		} else if (X.equals("Chips")) {
+			world.pickChips(num);
+		} else {
+			throw new Error("Unexpected parameter " + X);
+		}
+	}
+
+	private void put(String n, String X) {
+		int num = getValueFromString(n);
+		if (X.equals("Balloons")) {
+			world.putBalloons(num);
+		} else if (X.equals("Chips")) {
+			world.putChips(num);
+		} else {
+			throw new Error("Unexpected parameter " + X);
+		}
+	}
+
+	private void execute(StringBuffer out) {
+		try {
+			while (!opStack.empty()) {
+				String op = opStack.pop();
+				if (op.startsWith("assignVar")) {
+					String[] params = op.replace("assignVar", "").replace("(", "").replace(")", "").split(",");
+					assignVar(Integer.parseInt(params[0].trim()), params[1].trim());
+				} else if (op.startsWith("move")) {
+					String param = op.replace("move(", "").replace(")", "").trim();
+					move(param);
+				} else if (op.startsWith("turn")) {
+					String param = op.replace("turn(", "").replace(")", "").trim();
+					turn(param);
+				} else if (op.startsWith("face")) {
+					String param = op.replace("face(", "").replace(")", "").trim();
+					face(param);
+				} else if (op.startsWith("put")) {
+					String[] params = op.replace("put(", "").replace(")", "").split(",");
+					put(params[0].trim(), params[1].trim());
+				} else if (op.startsWith("pick")) {
+					String[] params = op.replace("pick(", "").replace(")", "").split(",");
+					pick(params[0].trim(), params[1].trim());
+				} else if (op.startsWith("moveDir")) {
+					String[] params = op.replace("moveDir(", "").replace(")", "").split(",");
+					moveDir(params[0].trim(), params[1].trim());
+				} else if (op.startsWith("moveInDir")) {
+					String[] params = op.replace("moveInDir(", "").replace(")", "").split(",");
+					moveInDir(params[0].trim(), params[1].trim());
+				} else if (op.startsWith("facing")) {
+					String param = op.replace("facing(", "").replace(")", "").trim();
+					facing(param);
+				} else {
+					String param = op.replace("not(", "").replace(")", "");
+					not(param);
+				}
+			}
+		} catch (Error e) {
+			out.append(e.getMessage());
+		}
 	}
 
 	/**
@@ -174,7 +413,7 @@ public class Interpreter {
 						if (input.equals("END")) {
 							ok = !ok;
 							inRoutine = !inRoutine;
-							//TODO Popear stack
+							execute(output);
 						}
 							break;
 					case 's':
@@ -184,18 +423,42 @@ public class Interpreter {
 							break;
 					case 'm':
 						if (input.startsWith("move")) {
-							String param = input.replace("move(", "").replace(")", "");
-							opStack.push("move(" + param + ")");
+							opStack.push(input);
 							ok = !ok;
 						}
+						if (input.startsWith("moveDir")) {
+							opStack.push(input);
+							ok = !ok;
+						}
+						if (input.startsWith("moveInDir"))
 							break;
 					case 'f':
 						if (input.startsWith("facing")) {
-							String param = input.replace("facing(", "").replace(")", "");
-							opStack.push("facing(" + param + ")");
+							opStack.push(input);
 							ok = !ok;
 						}
 							break;
+					case 'n':
+						if (input.startsWith("not")) {
+							opStack.push(input);
+							ok = !ok;
+						}
+							break;
+					case 't':
+						if (input.startsWith("turn")) {
+							opStack.push(input);
+							ok = !ok;
+						}
+							break;
+					case 'p':
+						if (input.startsWith("put")) {
+							opStack.push(input);
+							ok = !ok;
+						}
+						if (input.startsWith("pick")) {
+							opStack.push(input);
+							ok = !ok;
+						}
 					default:
 						output.append(" Unrecognized command:  " + input.charAt(i));
 						ok = false;
